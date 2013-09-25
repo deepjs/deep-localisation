6f95bbf8d07a0cacb026b81d46fac71e37799b43
Gilles Coomans <gilles.coomans@gmail.com>
2013-09-25 14:16:34 +0200

    work on patch

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ define(["require","deep/deep"],function (require, deep)
 	//__________________________________________________
 	deep.protocoles.translate = new deep.Store();
 
-	var filter = function (root, language, wrap) {
+  deep.protocoles.translate.map = {
+
+  }
+
+	var filter = function (root, language, wrap, localID) {
 	
 	   var res = {};
        var current = null;
@@ -44,7 +48,7 @@ define(["require","deep/deep"],function (require, deep)
                         
                    var va = v[i];
                    if(typeof va[language] === 'string')
-                        currentRes[i] = wrap?wrap(va[language], current.path+i):va[language];
+                        currentRes[i] = wrap?wrap(va[language], localID+current.path+i ):va[language];
                    else if(typeof va == "object")
                     {
                         if(va.push)
@@ -80,16 +84,19 @@ define(["require","deep/deep"],function (require, deep)
         lang = options.defaultLanguage;
     }
 		if(!lang)
-			return deep.errors.Protocole("No language available for translate protocole");
+			return deep.errors.Protocole("No language available for translate protocole (get)");
 		//console.log("swig store : ", id, options)
-    console.log("tranlate options : ", options.language());
+    //console.log("tranlate options : ", options.language());
+      
 		if(options.cache !== false && deep.mediaCache.cache["translate-" + lang + "::" +id])
 			return deep(deep.mediaCache.cache["translate-" + lang + "::" +id]).store(this);
 		var self = this;
 		var d = deep("json::" + id)
 		.done(function (data) {
 			//prendre la langue
-			var resi = filter(data, lang, options.wrap);
+      var localID = "src-"+new Date().valueOf()+"::";
+      deep.protocoles.translate.map[localID] = id;
+			var resi = filter(data, lang, options.wrap, localID);
 			//console.log("translate store : resi ", resi);
 			delete deep.mediaCache.cache["translate-" + lang + "::"+id];
 			if((options && options.cache !== false)  || (self.options && self.options.cache !== false))
@@ -101,9 +108,49 @@ define(["require","deep/deep"],function (require, deep)
 			deep.mediaCache.manage(d, "translate-" + lang + "::" +id);
 		return d;
 	};
+
+  deep.protocoles.translate.patch = function (value, options) {
+    var options = deep.protocoles.translate.options || {};
+      
+    if(options && options._deep_ocm_)
+        options = options();
+
+    var lang = null;
+    if(options)
+    {
+      if(options.language)
+          lang = options.language();
+      else if (options && options.defaultLanguage)
+        lang = options.defaultLanguage;
+    }
+    if(!lang)
+      return deep.errors.Protocole("No language available for translate protocole (patch)");
+    //console.log("swig store : ", id, options)
+    //console.log("tranlate options : ", options.language());
+      
+    if(options.cache !== false && deep.mediaCache.cache["translate-" + lang + "::" +id])
+      return deep(deep.mediaCache.cache["translate-" + lang + "::" +id]).store(this);
+    var self = this;
+    var d = deep("json::" + id)
+    .done(function (data) {
+      //prendre la langue
+      var localID = "src-"+new Date().valueOf()+"::";
+      deep.protocoles.translate.map[localID] = id;
+      var resi = filter(data, lang, options.wrap, localID);
+      //console.log("translate store : resi ", resi);
+      delete deep.mediaCache.cache["translate-" + lang + "::"+id];
+      if((options && options.cache !== false)  || (self.options && self.options.cache !== false))
+        deep.mediaCache.manage(resi, "translate-" + lang + "::" +id);
+      return resi;
+    })
+    .store(this);
+    if((options && options.cache !== false)  || (self.options && self.options.cache !== false))
+      deep.mediaCache.manage(d, "translate-" + lang + "::" +id);
+    return d;
+  };
 	
 	return function (opt) {
-		console.log("Options in tranlsate protocole = ", opt);
+		//console.log("Options in tranlsate protocole = ", opt);
 		deep.protocoles.translate.options = opt || {};
 	}
 });
