f56921369c3fa394c442f7be890e941001af6eac
Philippe Delabye <phil@chemicalrules.net>
2013-11-26 17:10:40 +0100

    refactoring deep/deep in deepjs/deep

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 if(typeof define !== 'function')
 	var define = require('amdefine')(module);
 
-define(["require","deep/deep"],function (require, deep)
+define(["require","deepjs/deep"],function (require, deep)
 {
-	var deep = require("deep/deep");
+	var deep = require("deepjs/deep");
 	//__________________________________________________
 	deep.protocoles.translate = new deep.Store();
 
