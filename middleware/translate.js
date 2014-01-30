39c3bba636a657d18c4f44d5ff31a21dc0509e3a
Philippe Delabye <phil@chemicalrules.net>
2014-01-30 19:29:55 +0100

    GET and PATCH in translate middleware

--- middleware/translate.js
+++ middleware/translate.js
@@ -5,7 +5,7 @@
 var deep = require("deepjs/deep");
 require("deep-mongo");
 
-deep.store.Mongo.create("translate", "mongodb://127.0.0.1:27017/smart-localisation", "translate",{
+var store = deep.store.Mongo.create("translate", "mongodb://127.0.0.1:27017/smart-localisation", "translate",{
 	properties:{
 		id:{ type:"string", required:false, indexed:true },
 		language:{ type:"string", required:true},
@@ -13,7 +13,6 @@ deep.store.Mongo.create("translate", "mongodb://127.0.0.1:27017/smart-localisati
 		translations:{ type:"object", required:true}
 	}
 });
-
 
 exports.middleware = function(initialiser){
 	return function (req, res, next)
@@ -30,9 +29,6 @@ exports.middleware = function(initialiser){
 		
 		splitted.shift();
 
-		var moduleName = splitted.shift();
-		var path = splitted.join("/");
-		//console.log("Tranlsate deep context = ", deep.context);
 		var roles = deep.context.modes.roles, isTranslator = false;
 		if(roles && roles.foreach)
 		{
@@ -45,9 +41,43 @@ exports.middleware = function(initialiser){
 				isTranslator = true;
 		}
 		isTranslator = true;
+
+		var moduleName = splitted.shift();
+		var path = splitted.join("/");
+
+		switch(req.method.toLowerCase())
+		{
+			case "get":
+				break;
+			case "patch":
+				var action = null;
+				if(isTranslator)
+					action = deep.store(store).get("?language=" + smart.country + "-" + lang + "&module=" + moduleName)
+					.done(function(results){
+						if(results.length === 0)
+							return deep.errors.NotFound();
+						var datas = results.shift();
+						this.patch(req.body,{ id:"/"+datas.id+"/translations/"+path });
+					});
+				else
+					action = deep.errors.Forbidden("you need to be translator to perform this operation.");
+				deep.when(action)
+				.done(function (success) {
+					res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8', "Location":req.url});
+					res.end(JSON.stringify(success.translations));
+				})
+				.fail(function(e){
+					console.log("translation error : ", e.toString());
+					res.writeHead(e.status || 400, {'Content-Type': 'text/html'});
+					res.end("error : "+JSON.stringify(e));
+				});
+				return;
+			default: throw deep.errors.MethodNotAllowed();
+		}
+
 		var query = "translate::?language=" + smart.country + "-" + lang + "&module=" + moduleName;
-		console.log("Translation Query = ", query);
-		deep(query).log()
+		//console.log("Translation Query = ", query);
+		deep(query)
 		.done(function (success) {
 
 			if(success.length === 0)
@@ -57,8 +87,8 @@ exports.middleware = function(initialiser){
 				{
 					if(lang != "en")
 					{//make a copy of en and create the new one
-						return deep("translate::?language=" + smart.country + "-en" +  "&module=" + moduleName).log("------Getting Default EN translations for creating " + lang)
-						.log()
+						return deep("translate::?language=" + smart.country + "-en" +  "&module=" + moduleName)
+						//.log("------Getting Default EN translations for creating " + lang)
 						.done(function (success) {
 							if(success.length === 0)
 								return deep.errors.NotFound(); // the module didnt exist yet
@@ -70,8 +100,12 @@ exports.middleware = function(initialiser){
 									module:moduleName,
 									translations:success.translations
 								}).done(function (success) {
-									console.log("creating default SUCCESS");
-									var rez = deep.query(success.translations, "/" + path);
+									//console.log("creating default SUCCESS");
+									var rez = null;
+									if(path === "")
+										rez = success.translations;
+									else
+										rez = deep.query(success.translations,  path);
 									res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8', "Location":req.url});
 									res.end(JSON.stringify(rez));
 								}).fail(function (e) {
@@ -91,70 +125,27 @@ exports.middleware = function(initialiser){
 			}
 			else
 			{
-				
 				success = success.shift();
-				console.log("Path for translation : ", path);
-				var rez = deep.query(success.translations, "/" + path);
-
+				//console.log("Path for translation : ", path);
+				var rez = null;
+				if(path === "")
+					rez = success.translations;
+				else
+					rez = deep.query(success.translations,  path);
+				// console.log("results for translation : ", rez);
 				res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8', "Location":req.url});
 				res.end(JSON.stringify(rez));
 			}
-
-
-
-			
-			// body...
 		}).fail(function (e) {
 			console.log("Fail to catch a translation");
 			res.writeHead(e.status || 400, {'Content-Type': 'text/html'});
 			res.end("error : "+JSON.stringify(e));
 		});
-		// if(translators[translator])
-		// {
-		// 	translator = translators[translator];
-		// 	var action = null;
 
-		// 	switch(req.method)
-		// 	{
-		// 		case "get":
-		// 			action= translator.get(path,lang);
-		// 			break;
-		// 		case "patch":
-		// 			if(isTranslator)
-		// 				action = translator.patch(path,lang);
-		// 			else
-		// 				action = deep.errors.Forbidden("you need to be translator to perform this operation.");
-		// 			break;
-		// 	}
-		// 	deep.when(action)
-		// 	.done(function (success) {
-		// 		res.writeHead(200, {'Content-Type': 'application/json;charset=utf-8', "Location":req.url});
-		// 		res.end(JSON.stringify(success));
-		// 	})
-		// 	.fail(function(e){
-		// 		console.log("translation error : ", e.toString());
-		// 		res.writeHead(e.status || 400, {'Content-Type': 'text/html'});
-		// 		res.end("error : "+JSON.stringify(e));
-		// 	});
-		// }
-		// else
-		// {
-		// 	console.log("translation error : Nothing found with translator : ", translator);
-		// 		res.writeHead(e.status || 400, {'Content-Type': 'text/html'});
-		// 		res.end("error : "+JSON.stringify(e));
-		// }
 		
 	};
 };
-// var Translator = exports.Translator = function (folderPath) {
-// 	this.stores = {};
-// };
 
-// Translator.prototype = {
-// 	get:function (path, lang) {
-// 		if(!this.stores[lang])
-// 			this.stores[lang] = deep.store.node.fs.Object.create(null, this.folderPath + lang + ".json");
-		
-// 		return this.stores[lang].get(path);
-// 	}
-// };
+
+
+
